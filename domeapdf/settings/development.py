45e13b931d8e3225cdbfdfc76baf01646516ce82
Krzysztof Mędrzycki <aiwenari@gmail.com>
2017-09-01 10:29:17 +0200

    Global app settings

---
+++ domeapdf/settings/development.py
@@ -0,0 +1,15 @@
+SECRET_KEY = '+)o(q-c@alk)22920@tl!-9j-vvqvghzibtv1u#lq6rvc+o9rf'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'FILE': ''
+  }
+}
---
+++ domeapdf/settings/production.py
@@ -0,0 +1,35 @@
+import os
+import toml
+
+_cfg = toml.load(os.environ['CONFIG'])
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = _cfg['secret_key']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = _cfg['allowed_hosts']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': _cfg['database']['name'],
+    'USER': _cfg['database']['user'],
+    'PASSWORD': _cfg['database']['password'],
+    'HOST': _cfg['database'].get('host', 'localhost'),
+    'PORT': _cfg['database'].get('port', ''),
+  }
+}
+
+ADMINS = _cfg['admins']
+
+# From django security checkup (./manage.py check --deploy)
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
