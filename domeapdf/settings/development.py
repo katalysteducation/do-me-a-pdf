5456f56ed8e2c960783e03a5dc5b757cdab05fb3
Krzysztof Mędrzycki <aiwenari@gmail.com>
2017-09-01 10:34:37 +0200

    Proper dev database settings

--- domeapdf/settings/development.py
+++ domeapdf/settings/development.py
@@ -1,3 +1,5 @@
+import os
+
 SECRET_KEY = '+)o(q-c@alk)22920@tl!-9j-vvqvghzibtv1u#lq6rvc+o9rf'
 
 DEBUG = True
@@ -10,6 +12,6 @@ ALLOWED_HOSTS = []
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3',
-    'FILE': ''
+    'NAME': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'db.sqlite3'),
   }
 }
