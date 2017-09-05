399d9cdc295984838c6b0fda2f0e870841847cad
Krzysztof Mędrzycki <aiwenari@gmail.com>
2017-09-05 15:05:24 +0200

    Added manage.py refreshstyles

---
+++ jobmgr/management/commands/refreshstyles.py
@@ -0,0 +1,32 @@
+import os
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from glob import glob
+
+from jobmgr.models import BookStyle
+
+class Command(BaseCommand):
+  help = 'Refresh list of PDF styles'
+
+  def add_arguments(self, parser):
+    pass
+
+  def handle(self, *args, **options):
+    def map_style(name):
+      return os.path.splitext(os.path.split(name)[-1])[0]
+
+    path = os.path.join(settings.CNX_OER_EXPORTS, 'css', 'ccap-*.css')
+    styles = list(map(map_style, glob(path)))
+
+    for style in set(map(lambda x: x.name, BookStyle.objects.all())).difference(styles):
+      style.delete()
+
+    for style in styles:
+      BookStyle.objects.get_or_create(name=style)
+
+    try:
+      default = BookStyle.objects.get(name='ccap-ked-university-physics')
+      default.default = True
+      default.save()
+    except BookStyle.DoesNotExist:
+      pass
